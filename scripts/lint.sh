8453e5fac98c991aeeda91874267a160c2ba7d6f
Patrick O'Neill <patrick.oneill@kensho.com>
2021-07-06 16:40:48 -0400

    debug mypy version in lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -155,6 +155,7 @@ fi
 
 if [ "$run_mypy" -eq 1 ]; then
     echo -e '*** Running mypy... ***\n'
+    echo `mypy --version`
     mypy $lintable_locations
     mypy_exit_code=$?
     echo -e "\n*** End of mypy run, exit: $mypy_exit_code ***\n"
