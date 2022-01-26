06bcb663cb8a7334ae1e196f0766f1507024a2b7
Georg Kucsko <gkucsko@gmail.com>
2022-01-26 08:03:50 -0500

    blank commit

--- pyctcdecode/tests/test_decoder.py
+++ pyctcdecode/tests/test_decoder.py
@@ -257,9 +257,9 @@ class TestDecoder(unittest.TestCase):
 
     def test_decode_batch(self):
         decoder = build_ctcdecoder(SAMPLE_LABELS, KENLM_MODEL_PATH, TEST_UNIGRAMS)
-        with multiprocessing.get_context("fork").Pool() as pool:
-            text_list = decoder.decode_batch(pool, [TEST_LOGITS] * 4)
-        expected_text_list = ["bugs bunny"] * 4
+        with multiprocessing.Pool() as pool:
+            text_list = decoder.decode_batch(pool, [TEST_LOGITS] * 5)
+        expected_text_list = ["bugs bunny"] * 5
         self.assertListEqual(expected_text_list, text_list)
 
     def test_logit_shape_mismatch(self):
@@ -273,7 +273,7 @@ class TestDecoder(unittest.TestCase):
 
     def test_decode_beams_batch(self):
         decoder = build_ctcdecoder(SAMPLE_LABELS, KENLM_MODEL_PATH, TEST_UNIGRAMS)
-        with multiprocessing.get_context("fork").Pool() as pool:
+        with multiprocessing.Pool() as pool:
             text_list = decoder.decode_beams_batch(pool, [TEST_LOGITS] * 5)
         expected_text_list = [
             [
