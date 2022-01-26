ab5e55b83ce995fa4ae67ab6b4b0f60bc8fc69ae
Georg Kucsko <gkucsko@gmail.com>
2022-01-26 07:55:45 -0500

    blank commit

--- pyctcdecode/tests/test_decoder.py
+++ pyctcdecode/tests/test_decoder.py
@@ -258,8 +258,8 @@ class TestDecoder(unittest.TestCase):
     def test_decode_batch(self):
         decoder = build_ctcdecoder(SAMPLE_LABELS, KENLM_MODEL_PATH, TEST_UNIGRAMS)
         with multiprocessing.get_context("fork").Pool() as pool:
-            text_list = decoder.decode_batch(pool, [TEST_LOGITS] * 5)
-        expected_text_list = ["bugs bunny"] * 5
+            text_list = decoder.decode_batch(pool, [TEST_LOGITS] * 4)
+        expected_text_list = ["bugs bunny"] * 4
         self.assertListEqual(expected_text_list, text_list)
 
     def test_logit_shape_mismatch(self):
